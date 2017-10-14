419f502f3c5d79bd7b991678e01870e70806c022
Andy Carlson <andy.carlson4@gmail.com>
2017-10-14 15:01:29 -0400

    added login/signup/logout funcitonality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
 	get 'pages/home'
 	root 'pages#home'
 	get "about" => "pages#about"
@@ -7,3 +8,4 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
